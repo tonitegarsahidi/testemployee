4450d3a7e46bdad3915b57d54bbf9824e497a84a
toni tegar sahidi <tonitegarsahidi@gmail.com>
2024-05-01 04:39:05 +0700

    make / route accessible for all even without auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(TestPylonBasicAuth::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
